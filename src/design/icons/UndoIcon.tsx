35aab9056f80a4621006124e3a6f3c3ea697a69c
machinellama <machinellama@proton.me>
2023-11-04 19:41:19 -0400

    0.4.1: restore and refresh icons

---
+++ src/design/icons/UndoIcon.tsx
@@ -0,0 +1,34 @@
+import { checkHex, omit } from '@util/index';
+import React, { HTMLAttributes } from 'react';
+
+interface Props extends HTMLAttributes<SVGElement> {
+  width?: number;
+  height?: number;
+  color?: string;
+}
+
+export function UndoIcon(props: Props) {
+  const strokeColor = checkHex(props.color) ? props.color : props.color ? `var(--${props.color})` : 'black';
+
+  // Added a transform property to flip the icon horizontally
+  const transform = 'scale(-1, 1)';
+
+  return (
+    <svg
+      tabIndex={0}
+      aria-label="left-icon"
+      {...omit(props, ['color'])}
+      width={props.width || 32}
+      height={props.height || 32}
+      viewBox="0 0 1024 1024"
+      xmlns="http://www.w3.org/2000/svg"
+      // Apply the transform to flip horizontally
+      transform={transform}
+    >
+      <path
+        fill={strokeColor}
+        d="M784.512 230.272v-50.56a32 32 0 1 1 64 0v149.056a32 32 0 0 1-32 32H667.52a32 32 0 1 1 0-64h92.992A320 320 0 1 0 524.8 833.152a320 320 0 0 0 320-320h64a384 384 0 0 1-384 384 384 384 0 0 1-384-384 384 384 0 0 1 643.712-282.88z"
+      />
+    </svg>
+  );
+}
