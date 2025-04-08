eeeb1858527f714a16fbc466538be86dc6878490
machinellama <machinellama@proton.me>
2025-04-07 23:23:30 -0400

    1.6.9: fix textinput empty value

--- src/components/inputs/input/TextInput.tsx
+++ src/components/inputs/input/TextInput.tsx
@@ -284,7 +284,7 @@ export function TextInput(props: TextInputProps) {
 
         {props.showClear &&
           !props.disabled &&
-          value &&
+          value != null &&
           (props.customClear ? (
             props.customClear
           ) : (
